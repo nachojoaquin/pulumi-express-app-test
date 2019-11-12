c70cabe2dabe8de7924a423d5e3ae727bdd432f8
ignacio.cavina <ignacio.cavina@globant.com>
2019-11-12 17:58:40 -0300

    now it works

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 "use strict";
 const awsx = require("@pulumi/awsx");
-const lambda = require('./lambda');
-const api = new awsx.apigateway.API("pulumitest", {
+const aws = require("@pulumi/aws");
+
+const eventHandler = new aws.lambda.CallbackFunction("pulumi-test", {
+    memorySize: 256,
+    codePathOptions: {
+        extraIncludePaths: ['./app.js']
+    },
+    callback: async (event, context) => {
+        const serverless = require('serverless-http');
+        const app = require('./app.js');
+        const handler = serverless(app);
+        const result = await handler(event, context);
+        return result;
+    },
+});
+
+const api = new awsx.apigateway.API("pulumi-test", {
     routes: [
         {
             path: "/",
             method: "ANY",
-            eventHandler: lambda.handler
+            eventHandler
         },
         {
             path: "/{proxy+}",
             method: "ANY",
-            eventHandler: lambda.handler
+            eventHandler
         },
     ],
+    stageName: 'dev'
 })
 
 module.exports = {
